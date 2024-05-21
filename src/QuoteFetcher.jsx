74f9d133c150b7009044d52db090811abc30fdeb
Milton3000 <kristofferssonmilton@gmail.com>
2024-05-21 15:29:43 +0200

    Second

---
+++ src/QuoteFetcher.jsx
@@ -0,0 +1,8 @@
+import React, { useState, useEffect } from "react";
+
+
+const QuoteFetcher = () => {
+    const [quotes, setQuotes] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+}
