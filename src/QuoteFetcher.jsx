3d4f2e84ab2b769721f7666daa740dd1650d6ec8
Milton3000 <kristofferssonmilton@gmail.com>
2024-05-21 15:31:40 +0200

    3rd

--- src/QuoteFetcher.jsx
+++ src/QuoteFetcher.jsx
@@ -5,4 +5,18 @@ const QuoteFetcher = () => {
     const [quotes, setQuotes] = useState([]);
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
+
+
+useEffect(() => {
+    const fetchQuote = async () => {
+        const response = await fetch('https://zenquotes.io/api/random');
+        if (!response.ok) {
+            throw new Error('Network response failed');
+        }
+        const data = await response.json();
+        return data[0]; 
+    };
+
+})
+
 }
